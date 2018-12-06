9277be16cd58e3dd47b9210dc10f4e83288799b9
qwerjunety <junemelodelachica@gmail.com>
2018-12-06 21:07:43 +0800

    Third commit

--- app/src/main/java/june/delachica/com/kwykfyx/HomeScreenProblem.java
+++ app/src/main/java/june/delachica/com/kwykfyx/HomeScreenProblem.java
@@ -1,7 +1,10 @@
 package june.delachica.com.kwykfyx;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class HomeScreenProblem extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class HomeScreenProblem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen_problem);
+        RelativeLayout queryOne = findViewById(R.id.query1);
+
+        queryOne.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i=  new Intent(HomeScreenProblem.this, HomeScreenProblemSolution.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/june/delachica/com/kwykfyx/MainActivity.java
+++ app/src/main/java/june/delachica/com/kwykfyx/MainActivity.java
@@ -1,10 +1,13 @@
 package june.delachica.com.kwykfyx;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -20,6 +23,31 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+        Button mainHome = findViewById(R.id.mainHome);
+        Button mainFind = findViewById(R.id.mainFind);
+        Button mainSearch = findViewById(R.id.mainSearch);
+        mainHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i=  new Intent(MainActivity.this, HomeScreen.class);
+                startActivity(i);
+            }
+        });
+        mainFind.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i=  new Intent(MainActivity.this, FindMechanic.class);
+                startActivity(i);
+            }
+        });
+        mainSearch.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i=  new Intent(MainActivity.this, ProblemScreen.class);
+                startActivity(i);
+            }
+        });
 
     }
 
