6f4324f4ee2855ed8056255b10100213425ad03a
qwerjunety <junemelodelachica@gmail.com>
2018-12-06 16:56:22 +0800

    Third commit

---
+++ app/src/main/java/june/delachica/com/kwykfyx/HomeScreenProblemSolution.java
@@ -0,0 +1,13 @@
+package june.delachica.com.kwykfyx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreenProblemSolution extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen_problem_solution);
+    }
+}
